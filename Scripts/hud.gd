2254062188c65e78c67e1b2a06706beac490a242
rusithhack <101166805+RusithWelisara@users.noreply.github.com>
2025-10-07 18:47:23 +0530

    pls fix this

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -3,11 +3,17 @@ extends CanvasLayer
 func _ready() -> void:
 	visible = true
 	GameManager.toggle_hud.emit(true)
+	$"Enter Name".visible = true
 
+#Main Menu
 
 func _on_start_game_pressed() -> void:
-	GameManager.toggle_hud.emit(false)
-	visible = false
+	if GameManager.player_name != "" :
+		GameManager.toggle_hud.emit(false)
+		visible = false
+	else:
+		$MainMenu.visible = false
+		$"Enter Name".visible = true
 
 func _on_leader_board_pressed() -> void:
 	get_tree().change_scene_to_file("res://addons/silent_wolf/Scores/Leaderboard.tscn")
@@ -15,3 +21,17 @@ func _on_leader_board_pressed() -> void:
 
 func _on_quit_game_pressed() -> void:
 	get_tree().quit()
+
+#Enter Name
+
+
+func _on_start_game_with_name_pressed() -> void:
+	if GameManager.player_name != "" :
+		GameManager.player_name = $"MainMenu/Enter Name/LineEdit".text
+		GameManager.toggle_hud.emit(false)
+		visible = false
+
+func _on_back_with_name_pressed() -> void:
+	visible = true
+	GameManager.toggle_hud.emit(true)
+	
