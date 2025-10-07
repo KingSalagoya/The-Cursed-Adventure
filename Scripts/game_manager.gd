4a74de73401a817d719c0fe5d586f44539fa5d9d
KingSalagoya <dalugodamalee@gmail.com>
2025-10-07 21:42:50 +0530

    fixed ui related issues, inconsistent node tree and saave game states and ending up with making leaderboard menu a bit more pixel-like

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -2,18 +2,49 @@ extends Node
 
 signal sacrifice_skill(name: String)
 signal toggle_hud(toggle: bool)
-signal win_sequence()
+signal start_game ()
+signal win_sequence ()
+
+var best_time              := 0
+var current_time           := 0
+var player_name            := ""
 
 var latest_check_point     := Vector2.ZERO
-var player: Player
-var best_time
-var player_name
-var score = 0
+var player                 : Player
+
+const save_path: String = "user://userdata.json"
+
+
+func _enter_tree() -> void:
+	load_game()
 
 func _ready() -> void:
 	RenderingServer.global_shader_parameter_set("VIEWPORT_SIZE", get_viewport().size)
 	print_debug(get_viewport().size)
 	print("hellow")
+
+func save_game() -> void:
+	var contents_to_save = {
+	"player_name": player_name,
+	"best_time": best_time
+	}
+
+	var file = FileAccess.open_encrypted_with_pass(save_path, FileAccess.WRITE, "dev_snapshot")
+	file.store_var(contents_to_save.duplicate())
+	file.close()
+
+
+func load_game() -> void:
+	if FileAccess.file_exists(save_path):
+		var file = FileAccess.open_encrypted_with_pass(save_path, FileAccess.READ, "dev_snapshot")
+		var data: Dictionary = file.get_var()
+		file.close()
+			
+		var save_data = data.duplicate()
+		
+		player_name = save_data.player_name
+		best_time = int(save_data.best_time)
+
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("exit"):
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,26 +1,34 @@
 extends Node
 
 @export var MUSIC: AudioStream
-@onready var timer: Timer = $UI/HUD/Main/MarginContainer/Timer/Timer
+
+@onready var speedrun_timer: Timer = $UI/HUD/Main/MarginContainer/Timer/Timer
 @onready var timer_label: Label = $UI/HUD/Main/MarginContainer/Timer/TimerContainer/TimerLabel
-var total_time_in_secs : int = 0
+
+
 
 func _ready() -> void:
 	GlobalAudioManager.play_music(MUSIC)
 	GameManager.win_sequence.connect(win_game)
-	timer.start()
+	GameManager.start_game.connect(start_game)
+
+func start_game() -> void:
+	speedrun_timer.start()
+
 
 func win_game() -> void:
 	$WinAnimationPlayer.play("win")
-	GameManager.best_time = timer_label.text
-	GameManager.score = GameManager.best_time
-	SilentWolf.Scores.save_score(GameManager.player_name, GameManager.score)
+	if GameManager.current_time < GameManager.best_time:
+		GameManager.current_time = GameManager.best_time
+		GameManager.save_game()
+		SilentWolf.Scores.save_score(GameManager.player_name, timer_label.text)
+
 
 func _on_quit_game_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
 
 func _on_timer_timeout() -> void:
-	total_time_in_secs += 1
-	var m = int(total_time_in_secs / 60.0)
-	var s = total_time_in_secs - m * 60
+	GameManager.current_time += 1
+	var m = int(GameManager.current_time / 60.0)
+	var s = GameManager.current_time - m * 60
 	timer_label.text = str('%02d:%02d' % [m , s])
