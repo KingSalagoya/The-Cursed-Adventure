5e734758bd60cde18f97253ef0c3b86789b815b5
rusithhack <101166805+RusithWelisara@users.noreply.github.com>
2025-10-10 16:33:13 +0530

    wfwfuyj

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -10,6 +10,10 @@ var dash_unlocked = false
 var double_jump_unlocked = false
 var inviibity_unlocked = false
 
+var dash_shards : int = 0 
+var double_jump_shards : int = 0
+var invisibility_shards : int =0
+
 var best_time              := 0
 var current_time           := 0
 var player_name            := ""
