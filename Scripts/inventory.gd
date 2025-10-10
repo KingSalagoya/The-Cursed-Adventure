21413756cf7003818956ee502222cbd2c2fc3e02
rusithhack <101166805+RusithWelisara@users.noreply.github.com>
2025-10-10 14:02:13 +0530

    inventory

--- Scripts/inventory.gd
+++ Scripts/inventory.gd
@@ -1,9 +1,30 @@
 extends Control
 
+var dash_hover = false
+var double_jump_hover = false
+var invisibility_hover = false
+
+
 func _ready() -> void:
-	$"Control/Dash Label".visible = true
+	$"Control/Dash Label".visible = false
 	$"Control/Double Jump Label".visible = false
 	$"Control/Invisibility Label".visible = false
+	$Control/msg.visible = true
+	
+	if GameManager.dash_unlocked == true:
+		$Control/VBoxContainer/SkillUI/ShadowStep/TextureRect.visible = false
+	else:
+		$Control/VBoxContainer/SkillUI/ShadowStep/TextureRect.visible = true
+	
+	if GameManager.double_jump_unlocked == true:
+		$Control/VBoxContainer/SkillUI/HighLeap/TextureRect2.visible = false
+	else:
+		$Control/VBoxContainer/SkillUI/HighLeap/TextureRect2.visible = true
+	
+	if GameManager.inviibity_unlocked == true:
+		$Control/VBoxContainer/SkillUI/Fade/TextureRect3.visible = false
+	else:
+		$Control/VBoxContainer/SkillUI/Fade/TextureRect3.visible = true
 	
 
 #hover
@@ -12,33 +33,33 @@ func _on_shadow_step_mouse_entered() -> void:
 	$"Control/Dash Label".visible = true
 	$"Control/Double Jump Label".visible = false
 	$"Control/Invisibility Label".visible = false
+	$Control/msg.visible = false
+	
 	
 func _on_high_leap_mouse_entered() -> void:
-	$"Control/Dash Label".visible = false
 	$"Control/Double Jump Label".visible = true
+	$"Control/Dash Label".visible = false
 	$"Control/Invisibility Label".visible = false
+	$Control/msg.visible = false
 
 func _on_fade_mouse_entered() -> void:
+	$"Control/Invisibility Label".visible = true
 	$"Control/Dash Label".visible = false
 	$"Control/Double Jump Label".visible = false
-	$"Control/Invisibility Label".visible = true
+	$Control/msg.visible = false
+
+#mouse exit
+
+func _on_shadow_step_mouse_exited() -> void:
+	$"Control/Dash Label".visible = false
+	$Control/msg.visible = true
 
 
-#usability
+func _on_high_leap_mouse_exited() -> void:
+	$"Control/Double Jump Label".visible = false
+	$Control/msg.visible = true
 
-func _usability():
-	if GameManager.dash_unlocked == true:
-		$Control/VBoxContainer/SkillUI/ShadowStep/TextureRect.visible = false
-	else:
-		$Control/VBoxContainer/SkillUI/ShadowStep/TextureRect.visible = true
-	
-	if GameManager.double_jump_unlocked == true:
-		$Control/VBoxContainer/SkillUI/ShadowStep/TextureRect.visible = false
-	else:
-		$Control/VBoxContainer/SkillUI/ShadowStep/TextureRect.visible = true
-	
-	if GameManager.invisibility == true:
-		$Control/VBoxContainer/SkillUI/ShadowStep/TextureRect.visible = false
-	else:
-		$Control/VBoxContainer/SkillUI/ShadowStep/TextureRect.visible = true
-	
+
+func _on_fade_mouse_exited() -> void:
+	$"Control/Invisibility Label".visible = false
+	$Control/msg.visible = true
