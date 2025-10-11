0814ce5b7bda9dece44f0e856f0667a3825e237d
KingSalagoya <dalugodamalee@gmail.com>
2025-10-12 01:13:58 +0530

    added

--- Scripts/HiddenAreas/area_trigger_resource.gd
+++ Scripts/HiddenAreas/area_trigger_resource.gd
@@ -1,5 +1,6 @@
 #YEAH I SUCK AT MAKING NAMES x2
 class_name AreaTriggerResource extends Resource
 
-@export var FADE_TIME          := 0.2
-@export var EXITING_IN_RIGHT   := false
+@export var FADE_TIME            := 0.2
+@export var EXITING_IN_RIGHT     := false
+@export var ROTATE_RIGHT_90      := false
