689a5a2dcf4cf10b0a3c5d00ddc1f57ac9077ba3
rusithhack <101166805+RusithWelisara@users.noreply.github.com>
2025-10-12 08:02:53 +0530

    egrgheg

--- Scripts/Globals/game_manager.gd
+++ Scripts/Globals/game_manager.gd
@@ -16,7 +16,8 @@ var inviibity_unlocked = false
 
 var dash_shards : int = 0 
 var double_jump_shards : int = 0
-var invisibility_shards : int =0
+var invisibility_shards : int = 0
+var curse_shards : int = 0
 
 var best_time              := 0
 var current_time           := 0
